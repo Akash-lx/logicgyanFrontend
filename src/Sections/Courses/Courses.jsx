d8c2836f1a376bb0f57a8043a164a1321984a695
AbhiLogicX <abhi.logicxhunt@gmail.com>
2024-10-17 16:55:43 +0530

    loader card issuem

--- src/Sections/Courses/Courses.jsx
+++ src/Sections/Courses/Courses.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@mui/material";
-import PreLoaderCard from "../../components/PerLoaderCard/preLoaderCard";
+import PreLoaderCard from "../../components/PerLoaderCard/PreLoaderCard";
 
 const Courses = () => {
   return (
